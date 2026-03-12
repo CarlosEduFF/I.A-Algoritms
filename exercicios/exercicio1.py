b7fa38cf9eada55cc214ad0f9ccb21aaf73385ec
CarlosEduFF <carloseduardoblackx@gmail.com>
2026-03-12 20:46:04 -0300

    Exercício 1 - Teste

---
+++ exercicios/exercicio1.py
@@ -0,0 +1,15 @@
+
+def exercicio1():
+    temps = [[24, 25, 26], [27, 28, 29], [21, 22, 25]]
+    cids = ["São Paulo", "Curitiba", "Rio de Janeiro"]
+
+    print("\nResultados:")
+    for j in range(3):
+        print(f"Cidade: {cids[j]}")
+        for i in range(3):
+            print(f"Temperatura: {temps[j][i]}")
+
+def calcularMédias(tem[]: float):
+
+    for x in range(3):
+        temps[1]  =+ temps[x]
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+
+from exercicios.exercicio1 import exercicio1   
+
+
 def main():
+    exercicio1()
 
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
